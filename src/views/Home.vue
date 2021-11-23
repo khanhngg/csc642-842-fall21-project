4523d1fc3047feaa5ec9e03e44874f8ff3f57d5f
Khanh Nguyen <knguyenn11@gmail.com>
2021-11-23 03:25:12 -0800

    Add vuelidate for form validations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
     <p class="form-text fst-italic mb-4">Required fields are marked with *</p>
 
     <!-- Form -->
-    <form class="row g-3" @submit.prevent="onSubmit">
+    <form class="row g-3" @submit.prevent="onSubmit" novalidate>
       <h4 class="fw-bold text-uppercase">Personal Information</h4>
       <!-- Last Name -->
       <div class="col-sm-6 mb-3">
@@ -17,10 +17,19 @@
           type="text"
           class="form-control"
           placeholder="Enter your last name"
-          maxlength="40"
-          required
+          :class="{ 'invalid-field': v$.lastName.$error }"
           v-model="lastName"
+          required
         />
+        <div v-if="v$.lastName.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.lastName.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <!-- First Name -->
@@ -31,15 +40,28 @@
           class="form-control"
           placeholder="Enter your first name"
           maxlength="40"
-          required
+          :class="{ 'invalid-field': v$.firstName.$error }"
           v-model="firstName"
         />
+        <div v-if="v$.firstName.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.firstName.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <!-- Preferred Title -->
       <div class="col-12 mb-3">
         <label class="form-label">Preferred Title *</label>
-        <select class="form-select" required v-model="preferredTitle">
+        <select
+          class="form-select"
+          v-model="preferredTitle"
+          :class="{ 'invalid-field': v$.preferredTitle.$error }"
+        >
           <option selected value="">Select your preferred title...</option>
           <option value="none">None</option>
           <option value="student">Student</option>
@@ -47,27 +69,56 @@
           <option value="staff">Staff</option>
           <option value="retired">Retired</option>
         </select>
+        <div v-if="v$.preferredTitle.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.preferredTitle.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <!-- Height -->
       <label class="form-label mb-0">Height</label>
-      <div class="col-12 col-md-3 mb-3 mt-0">
+      <div class="col-12 col-md-4 mb-3 mt-0">
         <div class="form-text">Feet</div>
         <input
           type="number"
           class="form-control"
           placeholder="Feet"
           v-model="height.feet"
+          :class="{ 'invalid-field': v$.height.feet.$error }"
         />
+        <div v-if="v$.height.feet.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.height.feet.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
-      <div class="col-12 col-md-3 mb-3 mt-0">
+      <div class="col-12 col-md-4 mb-3 mt-0">
         <div class="form-text">Inches</div>
         <input
           type="number"
           class="form-control"
           placeholder="Inches"
           v-model="height.inches"
+          :class="{ 'invalid-field': v$.height.inches.$error }"
         />
+        <div v-if="v$.height.inches.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.height.inches.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <!-- Phone -->
@@ -76,11 +127,19 @@
         <input
           type="tel"
           class="form-control"
-          placeholder="1234567890"
+          placeholder="Enter a 10 digit phone number"
           v-model="phone"
-          v-mask="'(###) ###-####'"
+          :class="{ 'invalid-field': v$.phone.$error }"
         />
-        <p class="form-text mb-0">Enter a 10 digit phone number</p>
+        <div v-if="v$.phone.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.phone.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <!-- Address -->
@@ -93,9 +152,18 @@
           class="form-control"
           placeholder="123 Main St"
           maxlength="40"
-          required
           v-model="address.streetAddress"
+          :class="{ 'invalid-field': v$.address.streetAddress.$error }"
         />
+        <div v-if="v$.address.streetAddress.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.address.streetAddress.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <div class="col-12">
@@ -106,7 +174,17 @@
             class="form-control"
             placeholder="20A"
             v-model="address.apartmentNumber"
+            :class="{ 'invalid-field': v$.address.apartmentNumber.$error }"
           />
+        </div>
+        <div v-if="v$.address.apartmentNumber.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.address.apartmentNumber.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
         </div>
       </div>
 
@@ -117,7 +195,17 @@
           class="form-control"
           placeholder="Enter city name"
           v-model="address.city"
+          :class="{ 'invalid-field': v$.address.city.$error }"
         />
+        <div v-if="v$.address.city.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.address.city.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <div class="col-md-5">
@@ -125,12 +213,21 @@
         <select
           class="form-select"
           id="country"
-          required
           v-model="address.country"
+          :class="{ 'invalid-field': v$.address.country.$error }"
         >
           <option value="">Select country...</option>
-          <option>United States</option>
+          <option value="United States">United States</option>
         </select>
+        <div v-if="v$.address.country.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.address.country.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <div class="col-md-4">
@@ -138,12 +235,21 @@
         <select
           class="form-select"
           id="state"
-          required
           v-model="address.state"
+          :class="{ 'invalid-field': v$.address.state.$error }"
         >
           <option value="">Select state...</option>
           <option>California</option>
         </select>
+        <div v-if="v$.address.state.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.address.state.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <div class="col-md-3 mb-3">
@@ -154,9 +260,18 @@
           id="zip"
           placeholder="12345"
           maxlength="5"
-          required
           v-model="address.zip"
+          :class="{ 'invalid-field': v$.address.zip.$error }"
         />
+        <div v-if="v$.address.zip.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.address.zip.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <!-- Services -->
@@ -261,9 +376,18 @@
           type="email"
           class="form-control"
           placeholder="you@example.com"
-          required
           v-model="email"
+          :class="{ 'invalid-field': v$.email.$error }"
         />
+        <div v-if="v$.email.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.email.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <!-- Terms and Conditions -->
@@ -273,24 +397,49 @@
           <input
             type="checkbox"
             class="form-check-input"
-            required
             v-model="terms"
+            :class="{ 'invalid-field': v$.terms.$error }"
           />
-          <label class="form-check-label form-label">I agree to the <a href="#">Terms & Conditions</a> *</label>
+          <label class="form-check-label form-label mb-0"
+            >I agree to the <a href="#">Terms & Conditions</a> *</label
+          >
+        </div>
+        <div v-if="v$.terms.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.terms.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
         </div>
       </div>
 
       <!-- Captcha -->
       <div class="mb-5">
         <label class="form-label">CAPTCHA *</label>
-        <input type="text" class="form-control" />
+        <input
+          type="text"
+          class="form-control"
+          v-model="captcha"
+          :class="{ 'invalid-field': v$.captcha.$error }"
+        />
+        <div v-if="v$.captcha.$error">
+          <p
+            :key="error.$uid"
+            v-for="error in v$.captcha.$errors"
+            class="mb-0 invalid-message"
+          >
+            {{ error.$message }}
+          </p>
+        </div>
       </div>
 
       <!-- Button -->
       <div class="mb-5 text-center">
         <button
           class="w-50 btn btn-primary btn-lg fw-bold text-uppercase"
-          type="button"
+          type="submit"
         >
           Submit
         </button>
@@ -300,15 +449,29 @@
 </template>
 
 <script>
-import { mask } from "vue-the-mask";
+import useVuelidate from "@vuelidate/core";
+import {
+  required,
+  email,
+  minLength,
+  maxLength,
+  minValue,
+  maxValue,
+  alpha,
+  numeric,
+  sameAs,
+  helpers,
+} from "@vuelidate/validators";
+
+const validAlphaNumeric = (value) => {
+  return /^[a-zA-Z0-9 ]*$/.test(value)
+}
 
 export default {
   name: "Home",
-  directives: {
-    mask,
-  },
   data() {
     return {
+      v$: useVuelidate(),
       lastName: "",
       firstName: "",
       preferredTitle: "",
@@ -332,11 +495,86 @@ export default {
       captcha: false,
     };
   },
+  validations() {
+    return {
+      lastName: {
+        required,
+        alpha,
+        maxLength: maxLength(40),
+      },
+      firstName: {
+        required,
+        alpha,
+        maxLength: maxLength(40),
+      },
+      preferredTitle: { required },
+      height: {
+        feet: {
+          minValue: minValue(0),
+          maxValue: maxValue(10),
+        },
+        inches: {
+          minValue: minValue(0),
+          maxValue: maxValue(11),
+        },
+      },
+      phone: {
+        numeric,
+        maxLength: maxLength(10)
+      },
+      address: {
+        streetAddress: {
+          required,
+          validAlphaNumeric: helpers.withMessage('The value must be alpha-numeric', validAlphaNumeric),
+          maxLength: maxLength(40),
+        },
+        apartmentNumber: {
+          validAlphaNumeric: helpers.withMessage('The value must be alpha-numeric', validAlphaNumeric),
+          maxLength: maxLength(40),
+        },
+        city: {
+          required,
+          validAlphaNumeric: helpers.withMessage('The value must be alpha-numeric', validAlphaNumeric),
+        },
+        country: {
+          required,
+          validAlphaNumeric: helpers.withMessage('The value must be alpha-numeric', validAlphaNumeric),
+        },
+        state: {
+          required,
+          validAlphaNumeric: helpers.withMessage('The value must be alpha-numeric', validAlphaNumeric),
+        },
+        zip: {
+          required,
+          numeric,
+          maxLength: maxLength(5),
+          minLength: helpers.withMessage('This field should have 5 digits', minLength(5)),
+        },
+      },
+      email: {
+        required,
+        email
+      },
+      terms: {
+        required,
+        sameAs: helpers.withMessage('Please indicate that you have read and agreed to the Terms & Conditions', sameAs(true)),
+      },
+      captcha: { required },
+    };
+  },
   methods: {
-    onSubmit() {
+    async onSubmit() {
       // validate
-      // save to local storage
-      // go to /result
+      console.log("onsubmit called...");
+      await this.v$.$touch();
+
+      if (!this.v$.$invalid) {
+        // save to local storage
+        // go to /result
+        console.log("no errors!! go to result...");
+      } else {
+        console.log("got errors");
+      }
     },
   },
 };
@@ -345,5 +583,20 @@ export default {
 <style scoped>
 .form-label {
   font-weight: 600;
+}
+
+.invalid-message {
+  margin-top: 0.25rem;
+  font-size: 0.875em;
+  color: #dc3545;
+}
+
+.invalid-field {
+  border-color: #dc3545;
+}
+
+.invalid-field:focus {
+  border-color: #dc3545;
+  box-shadow: 0 0 0 0.25rem rgb(220 53 69 / 25%);
 }
 </style>
